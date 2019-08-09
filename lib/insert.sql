730ec43e1401b329949db937586d335ff5aedad1
Tracy Reuther <tracyreuther@gmail.com>
2019-08-09 23:48:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
   
