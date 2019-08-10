e735baef59bb33377b6b6c8ed431a00400b97fda
Tracy Reuther <tracyreuther@gmail.com>
2019-08-10 00:57:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_series_title_with_most_human_characters
       INNER JOIN characters
         ON series.id = characters.series_id
       WHERE characters.species = 'human'
-      GROUP BY characters.series_id
+      GROUP BY title
       ORDER BY num_species DESC
       LIMIT 1);"
 end
