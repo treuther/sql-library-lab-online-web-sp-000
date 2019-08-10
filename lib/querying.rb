69817aaa29708df0c583a5c0e7e9749dd3ea6ba5
Tracy Reuther <tracyreuther@gmail.com>
2019-08-10 00:04:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.titles, books.year FROM books JOIN series ON "
+  "SELECT books.titles, books.year FROM books JOIN series ON books.series_id = series.id GROUP BY "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
