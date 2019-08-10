2c4482f2233b66a7a959b31193414e157377ed48
Tracy Reuther <tracyreuther@gmail.com>
2019-08-10 01:06:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name FROM"
+  "SELECT characters.name FROM characters JOIN character_books ON character_books.character_id = characters.id JOIN books ON character_books.book_id = books.id;"
 end
