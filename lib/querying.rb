748088fd0bb4ec5453de2419a0f2a645e997cd5e
Tracy Reuther <tracyreuther@gmail.com>
2019-08-10 01:07:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) AS num_books FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name;"
+  "SELECT characters.name, COUNT(character_books.book_id) AS num_books FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY num_books DESC;"
 end
